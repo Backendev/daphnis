f0420d88c2d3fea530b9382602311e18766a5ea9
Edward <backender.dev@gmail.com>
2022-02-18 13:19:02 -0500

    add github api

--- src/components/PrincipalComponent.vue
+++ src/components/PrincipalComponent.vue
@@ -9,11 +9,6 @@
 
 <script>
 var Octokat = require('octokat')
-//const { Octokit, App } = require("octokit");
-// const octo = new Octokat({
-//   token: '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',
-//   acceptHeader: 'application/vnd.github.cannonball-preview+json'
-// })
 export default {
    
   name: 'Principal',
@@ -32,15 +27,15 @@ export default {
  
     octo.zen.read(cb)
     const repo = octo.repos('muluk-code','dbdaphnis')
-    repo.contents('db.json').read() // Use `.read` to get the raw file.
-    .then((contents) => {        // `.fetch` is used for getting JSON
+    repo.contents('db.json').read()
+    .then((contents) => {
       this.data = contents
     });
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h1, h2 {
   font-weight: normal;
