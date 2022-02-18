18d065c3eeb1ad80ccf6b4790938c4d32cb2ce03
Edward <backender.dev@gmail.com>
2022-02-18 13:13:01 -0500

    add github api

--- src/components/PrincipalComponent.vue
+++ src/components/PrincipalComponent.vue
@@ -2,15 +2,40 @@
   #contenedor
     div
       | {{ msg }}
+      br
+      | Datos:
+      | {{ data }}
 </template>
 
 <script>
+var Octokat = require('octokat')
+//const { Octokit, App } = require("octokit");
+// const octo = new Octokat({
+//   token: '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',
+//   acceptHeader: 'application/vnd.github.cannonball-preview+json'
+// })
 export default {
+   
   name: 'Principal',
   data () {
     return {
-      msg: 'Proy Daphnis'
+      msg: 'Proy Daphnis',
+      data: {}
     }
+  },
+  mounted() {
+    const octo = new Octokat({
+      username: "mulukcode@gmail.com",
+      password: "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"
+    })
+    var cb = function (err, val) { console.log(val) }
+ 
+    octo.zen.read(cb)
+    const repo = octo.repos('muluk-code','dbdaphnis')
+    repo.contents('db.json').read() // Use `.read` to get the raw file.
+    .then((contents) => {        // `.fetch` is used for getting JSON
+      this.data = contents
+    });
   }
 }
 </script>
