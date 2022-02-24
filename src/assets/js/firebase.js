ceff64b43e2e70ef33990b29d45ebbdcf9ed0179
Edward <backender.dev@gmail.com>
2022-02-23 19:06:47 -0500

    firebase get set

--- src/assets/js/firebase.js
+++ src/assets/js/firebase.js
@@ -4,7 +4,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection as co, getDocs , query, where,setDoc,doc} from 'firebase/firestore';
 import { getDatabase,ref, child, get } from "firebase/database";
 
-    const firebaseConfig = {
+const db_fb = {}
+
+
+
+//const collection = co(db,'/categorias')
+
+// const q = query(co(db, "categorias"), where("name", "==", "python"));
+
+db_fb.get_categorias = function(){
+    let firebaseConfig = {
         apiKey: "AIzaSyBCuYo9UyiOJbcRw3pAvPKUP0Fz67YMvQY",
         authDomain: "daphne-d6497.firebaseapp.com",
         projectId: "daphne-d6497",
@@ -13,14 +22,46 @@ import { getDatabase,ref, child, get } from "firebase/database";
         appId: "1:866108407577:web:439988a15d75b636cd0f88",
         databaseURL: "https://categorias.firebaseio.com",
         }
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app)
-    const collection = co(db,'/categorias')
-
-    // const q = query(co(db, "categorias"), where("name", "==", "python"));
-    const q = query(co(db, "categorias"));
-    const querySnapshot = getDocs(q);
+    let app = initializeApp(firebaseConfig);
+    let db = getFirestore(app)
+    let q = query(co(db, "categorias"));
+    let querySnapshot = getDocs(q);
     let data = []
+    querySnapshot.then(
+        q => {
+            //console.log(q)
+            let get_data = q
+            get_data.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                    //console.log(doc.id, " => ", doc.data());
+                    let item = {"id":doc.id,"data":doc.data(),"o":"desde fun"}
+                    data.push(item)
+                  });
+        }
+    ).catch((error) => {
+        console.error(error);
+    });
+    return data
+}
+
+
+db_fb.set_categoria= function(name_c){
+    let firebaseConfig = {
+        apiKey: "AIzaSyBCuYo9UyiOJbcRw3pAvPKUP0Fz67YMvQY",
+        authDomain: "daphne-d6497.firebaseapp.com",
+        projectId: "daphne-d6497",
+        storageBucket: "daphne-d6497.appspot.com",
+        messagingSenderId: "866108407577",
+        appId: "1:866108407577:web:439988a15d75b636cd0f88",
+        databaseURL: "https://categorias.firebaseio.com",
+        }
+    let app = initializeApp(firebaseConfig);
+    let db = getFirestore(app)
+    let q = query(co(db, "categorias"));
+    let querySnapshot = getDocs(q);
+    let data = []
+    let l_data = 0
+    let datas = null
     querySnapshot.then(
         q => {
             console.log(q)
@@ -31,18 +72,21 @@ import { getDatabase,ref, child, get } from "firebase/database";
                     let item = {"id":doc.id,"data":doc.data(),"o":"desde fun"}
                     data.push(item)
                   });
+            l_data = data.length
+            let d = doc(db,"categorias",String(l_data + 1))
+            setDoc(d,{name:name_c}).then(
+                r =>{
+                    datas = db_fb.get_categorias()
+                    return "22"
+                }
+            )
+            
+            
         }
     ).catch((error) => {
         console.error(error);
     });
-    //Guardar
-    const l_data = data.length
-    let d = doc(db,"categorias",String(l_data + 1))
-    setDoc(d,{name:"javascript"})
-    // set_d.then(
-    //     s =>{
-    //         s = {name:"javascript"}
-    //     }
-    // )
+}
 
-export default data
+
+export default db_fb
